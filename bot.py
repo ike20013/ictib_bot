351bd147e645cccbe06bad92669708f63712f672
krinjmann <tavklandre@gmail.com>
2020-10-22 02:07:48 +0300

    test2

--- bot.py
+++ bot.py
@@ -2,13 +2,12 @@ import os
 import telebot
 from telebot import types
 from flask import Flask, request
-import config
-import match
 import requests
 import json 
 import datetime
 
 TOKEN = '1271897314:AAG1c6rZ8JnGtRA2Mx1mJ1A7LOHCo_-ysVY'
+
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
@@ -327,14 +326,15 @@ def get_user_group(user_id):
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-   print("getmessage success")
    return "!", 200
+
+
 @server.route("/")
 def webhook():
    bot.remove_webhook()
    bot.set_webhook(url='https://damp-depths-71393.herokuapp.com/' + TOKEN)
-   print("webhook start")
    return "!", 200
+
+
 if __name__ == "__main__":
    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-   print("server start")
