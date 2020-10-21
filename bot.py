22853f779455fdeab256dab0a8aeb4831ef8608d
krinjmann <tavklandre@gmail.com>
2020-10-22 01:51:34 +0300

    test2

--- bot.py
+++ bot.py
@@ -324,14 +324,17 @@ def get_user_group(user_id):
 
 
 # SERVER SIDE 
-@server.route('/' + config.token, methods=['POST'])
+@server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+   print("getmessage success")
    return "!", 200
 @server.route("/")
 def webhook():
    bot.remove_webhook()
-   bot.set_webhook(url='https://ictibochka.herokuapp.com/' + TOKEN)
+   bot.set_webhook(url='https://damp-depths-71393.herokuapp.com/' + TOKEN)
+   print("webhook start")
    return "!", 200
 if __name__ == "__main__":
    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+   print("server start")
